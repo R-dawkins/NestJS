665435a0fbf2ba6fbef9af52a2396f59555be37c
R-dawkins <tig03027@naver.com>
2025-07-15 17:44:27 +0900

    section 8 : How to use TypeOrm -complete-

--- mycv/src/main.ts
+++ mycv/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  // whitelist:true = 사용자가 요청으로 body에 추가 속성을 보내도 (예를들어 admin:true) 그를 자동으로 무시하는 보안 기능
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
